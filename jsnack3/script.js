ebe489e6965fc502f3973e0adc18886ea40eb76a
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-04-18 17:58:11 +0200

    fix "jsnack3"

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,10 +1,10 @@
 // ADD LIST OF NUMBER
-const numberList = [5, 10, 50, 120, 20, 90, 30, 20, 90, 20];
+const numberList = [2, 10, 50, 120, 20, 90, 30, 20, 90, 20];
 
 // SUM THE NUMBER IN ODD POSITION
 let sum = 0;
 for (let i = 0; i < numberList.length; i++) {
-  if (i % 2 !== 0) {
+  if (i % 2 !== 1) {
     sum += numberList[i];
   }
 }
