ff4b55c9f0537b054d0ca20513990f11ac6d711c
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-04-19 10:45:20 +0200

    fix "jsnack3" if

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,9 +1,9 @@
 // ADD LIST
 const guestName = ["Emma", "Marco", "Paolo", "Giorgia", "Enrico", "Giuseppe"];
 const guestSurname = ["Conte", "Meloni", "Zaia", "Letta", "Renzi", "Bonino"];
+const fakeList = [];
 
 // ADD CICLE FOR THE NEW ARRAY
-let fakeList = [];
 for (let i = 0; i < 6; i++) {
   let randomIndexName = Math.floor(Math.random() * guestName.length);
   let randomName = guestName[randomIndexName];
--- jsnack3/script.js
+++ jsnack3/script.js
@@ -4,7 +4,7 @@ const numberList = [2, 10, 50, 120, 20, 90, 30, 20, 90, 20];
 // SUM THE NUMBER IN ODD POSITION
 let sum = 0;
 for (let i = 0; i < numberList.length; i++) {
-  if (i % 2 !== 1) {
+  if (i % 2 == 1) {
     sum += numberList[i];
   }
 }
